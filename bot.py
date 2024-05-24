af4abda3f67e73306e6d4ba64004bdc0d9bc1fc9
spai <90276502+0xSpai@users.noreply.github.com>
2024-05-24 20:16:24 +0200

    Resolution change

--- bot.py
+++ bot.py
@@ -25,12 +25,12 @@ def rgb2hex(pixel):
     return '{:02x}{:02x}{:02x}'.format(*pixel)
 
 # Function to simulate a mouse click on a pixel
-def click_pixel(add_x, add_y, num_clicks=2):
+def click_pixel(add_x, add_y, num_clicks=3):
     click_x = round(firstX + add_x * (lastX - firstX) / 31)
     click_y = round(firstY + add_y * (lastY - firstY) / 31)
     
     for _ in range(num_clicks):
-        time.sleep(.0001)
+        time.sleep(.001)
         click(click_x, click_y)
 
 # Function to select a color in the game
@@ -39,6 +39,7 @@ def selectColor(color):
     click(openButtonX, openButtonY)
     click(inputX, inputY)
     vkey.typer(string=hexColor)
+    click(closeButtonX, closeButtonY)
     click(closeButtonX, closeButtonY)
     
 
@@ -92,12 +93,12 @@ roblox_window = roblox_windows[0]
 if roblox_window.isMinimized:
     roblox_window.restore()
 
-roblox_window.resizeTo(1280, 720)
-roblox_window.moveTo(center_x - 640, center_y - 360)
+roblox_window.resizeTo(1681, 957)
+roblox_window.moveTo(center_x - round(840.5), center_y - round(478.5))
 hwnd = roblox_window._hWnd
 win32gui.SetForegroundWindow(hwnd)
 
-if roblox_window.size != (1280, 720):
+if roblox_window.size != (1681, 957):
     print("Something went wrong. Try maximizing the Roblox window.")
     quit()
 
@@ -107,11 +108,11 @@ roblox_middle_x = (roblox_rect[0] + roblox_rect[2]) // 2
 roblox_middle_y = (roblox_rect[1] + roblox_rect[3]) // 2
 
 # Define action coordinates
-firstX, firstY = roblox_middle_x - 200, roblox_middle_y - 225
-lastX, lastY = roblox_middle_x + 200, roblox_middle_y + 170
-openButtonX, openButtonY = roblox_middle_x + 83, roblox_middle_y + 205
-inputX, inputY = roblox_middle_x + 83, roblox_middle_y + 150
-closeButtonX, closeButtonY = roblox_middle_x + 240, roblox_middle_y - 40
+firstX, firstY = roblox_middle_x - 260, roblox_middle_y - 300
+lastX, lastY = roblox_middle_x + 260, roblox_middle_y + 230
+openButtonX, openButtonY = roblox_middle_x + 110, roblox_middle_y + 285
+inputX, inputY = roblox_middle_x + 110, roblox_middle_y + 200
+closeButtonX, closeButtonY = roblox_middle_x + 310, roblox_middle_y - 45
 
 pixels = {}
 for x in range(32):
