81aa4b79d10cd1ca9059530332284843a69d1ee3
0xSpai <spairoblox@gmail.com>
2024-06-05 21:08:58 +0200

    Version 3

--- modules/painting.py
+++ modules/painting.py
@@ -1,3 +1,4 @@
+import os
 import time
 import win32api
 import win32con
@@ -8,7 +9,7 @@ from tqdm import tqdm
 from modules.window_management import setup_window
 from modules.utilities import verify_color
 
-# Function to simulate a mouse click at given coordinates
+# Simulate a mouse click at given coordinates
 def click(x, y):
     win32api.SetCursorPos((x, y))
     for _ in range(2):
@@ -18,7 +19,7 @@ def click(x, y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
     time.sleep(0.01)
 
-# Function to simulate a mouse click on a pixel
+# Simulate a mouse click on a pixel
 def click_pixel(coords, add_x, add_y, num_clicks=3):
     click_x = round(coords["firstX"] + add_x * (coords["lastX"] - coords["firstX"]) / 31)
     click_y = round(coords["firstY"] + add_y * (coords["lastY"] - coords["firstY"]) / 31)
@@ -36,33 +37,29 @@ def select_color(coords, color):
     for _ in range(2):
         click(coords["closeButtonX"], coords["closeButtonY"])
 
-
+# Start the painting process with the selected 32x32 image
 def start_painting(image_pixels, image_name):
     output.printAscii()
-    print("   Image selected:", image_name)
-    startInput = input("   Begin painting? (y/n) ")
-    if startInput.lower() != 'y':
+    print(f"   Image selected: {image_name}")
+    start_input = input("   Begin painting? (y/n) ").lower()
+    if start_input != 'y':
         output.clear()
         quit()
-    
+
     output.printAscii()
     print("Painting progress:")
     coords = setup_window()
 
     if coords is None:
-       output.printError("Something went wrong. Try again.")
+        output.printError("Something went wrong. Try again.")
+        return
     
-    pixels = {}
-    for x in range(32):
-        for y in range(32):
-            color = image_pixels[x, y]
-            if color != (255, 255, 255):
-                pixels.setdefault(color, []).append((x, y))
+    pixels = {(x, y): color for x in range(32) for y in range(32) if (color := image_pixels[x, y]) != (255, 255, 255)}
     
     time.sleep(1)
     for _ in range(2):
         click(coords["closeButtonX"], coords["closeButtonY"])
-    time.sleep(0.5)
+        time.sleep(0.5)
     click(coords["firstX"] + 530, coords["firstY"] + 590)
     time.sleep(0.5)
     click(coords["openButtonX"], coords["openButtonY"] - 205)
@@ -77,5 +74,5 @@ def start_painting(image_pixels, image_name):
             if not verify_color(click_x, click_y, color):
                 select_color(coords, color)
                 click_pixel(coords, *pixel)
-    
+
     print("\nPainting completed, enjoy!")
